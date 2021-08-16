af8110cac5ab748d4b3946200ee86a149bdff9a6
Industry Dive <bomorin-id@macbook-pro.myfiosgateway.com>
2021-08-16 12:55:15 -0400

    Oops

--- settings/prod.py
+++ settings/prod.py
@@ -1,7 +1,6 @@
 import os
 from settings import *  # noqa
 import dj_database_url
-import urllib2
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
